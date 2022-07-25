d0e95274c8deeb9d824bf50c9ea5d32e29c0503f
Asmasultana786 <104421115+Asmasultana786@users.noreply.github.com>
2022-07-25 11:27:13 +0530

    Create circle.js

---
+++ circle.js
@@ -0,0 +1,9 @@
+//import using require
+var Shape = require("./shape");
+//declare class
+class Circle extends Shape{
+  constructor()
+  {super();this.calculateArea=()=>{}}
+};
+module.exports=circle;
+//exports class using module.exports
